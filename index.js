667082a2d35d06f8be2d63b5191314d5dd7e373b
vhxubo <vhxubo@gmail.com>
2025-05-09 17:33:22 +0800

    chore: init project

---
+++ index.js
@@ -0,0 +1,43 @@
+import { createFilter } from "@rollup/pluginutils";
+
+export default function prefixLocalStorage(options) {
+  const {
+    prefix = "_",
+    include = ["**/*.js", "**/*.ts", "**/*.jsx", "**/*.tsx"],
+    exclude = ["**/node_modules/**", "**/dist/**"],
+  } = options || {};
+
+  const filter = createFilter(include, exclude);
+
+  return {
+    name: "prefix-localstorage",
+    transform(code, id) {
+      if (!filter(id)) {
+        return null;
+      }
+
+      const prefixedCode = code
+        .replace(
+          /localStorage\.getItem\((['"`])(.*?)\1\)/g,
+          `localStorage.getItem($1${prefix}$2$1)`,
+        )
+        .replace(
+          /localStorage\.setItem\((['"`])(.*?)\1,\s/g,
+          `localStorage.setItem($1${prefix}$2$1, `,
+        )
+        .replace(
+          /localStorage\.removeItem\((['"`])(.*?)\1\)/g,
+          `localStorage.removeItem($1${prefix}$2$1)`,
+        )
+        .replace(
+          /localStorage\.key\((['"`])(.*?)\1\)/g,
+          `localStorage.key($1${prefix}$2$1)`,
+        );
+
+      return {
+        code: prefixedCode,
+        map: null,
+      };
+    },
+  };
+}
